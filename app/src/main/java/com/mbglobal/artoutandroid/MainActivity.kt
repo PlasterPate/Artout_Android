326b0d8b3083d83bb41c35a02fac9ded716ee7f2
dalisyron <dalisyron@gmail.com>
2019-10-25 23:09:16 +0330

    Add simple Login view

--- app/src/main/java/com/mbglobal/artoutandroid/MainActivity.kt
+++ app/src/main/java/com/mbglobal/artoutandroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mbglobal.artoutandroid
 
 import android.os.Bundle
+import com.mbglobal.artoutandroid.ui.login.LoginFragment
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -8,5 +9,10 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContentFrame, LoginFragment())
+            .addToBackStack("Login")
+            .commit()
     }
 }
---
+++ app/src/main/java/com/mbglobal/artoutandroid/ui/login/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.mbglobal.artoutandroid.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mbglobal.artoutandroid.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
