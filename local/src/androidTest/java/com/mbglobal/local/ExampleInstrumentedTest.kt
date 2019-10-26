28843d682073e054b61f08d29934e036c4d0f350
dalisyron <dalisyron@gmail.com>
2019-10-27 00:30:30 +0330

    Expose local apis to app through new module named local

--- local/src/androidTest/java/com/mbglobal/local/ExampleInstrumentedTest.kt
+++ local/src/androidTest/java/com/mbglobal/local/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package com.mbglobal.local
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
