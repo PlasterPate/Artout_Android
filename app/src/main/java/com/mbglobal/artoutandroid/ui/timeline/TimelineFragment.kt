d3072d970126b8e0aebb9a590f1496045771525f
Ali Mohammad Movahedian <am.movahedian1998@gmail.com>
2019-11-09 04:15:49 +0330

    Add new icon for add event fab

--- app/src/main/java/com/mbglobal/artoutandroid/ui/timeline/TimelineFragment.kt
+++ app/src/main/java/com/mbglobal/artoutandroid/ui/timeline/TimelineFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mbglobal.artoutandroid.R
@@ -15,6 +17,9 @@ import com.mbglobal.artoutandroid.ui.base.BaseFragment
 class TimelineFragment : BaseFragment() {
 
     lateinit var binding : FragmentTimelineBinding
+
+    @Inject
+    lateinit var viewModel: TimelineViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +34,7 @@ class TimelineFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
--- data/src/main/java/com/mbglobal/data/repository/EventRepository.kt
+++ data/src/main/java/com/mbglobal/data/repository/EventRepository.kt
@@ -15,26 +15,6 @@ class EventRepository @Inject constructor(
     private val  userLocalDataSource: UserLocalDataSource) {
 
     fun getEvent(eventId : Int) : Single<EventEntity> {
-//        if (eventId == 1) {
-//            val image = "https://imgs.6sqft.com/wp-content/uploads/2019/10/02112923/Central-Park-fall-foliage-2019.jpg"
-//            val location = LocationEntity(
-//                12.0,
-//                10.0
-//            )
-//
-//            val event = EventEntity(
-//                "This is the event description",
-//                "2019-01-01",
-//                1,
-//                location,
-//                "https://imgs.6sqft.com/wp-content/uploads/2019/10/02112923/Central-Park-fall-foliage-2019.jpg",
-//                "2020-01-02",
-//                "Title",
-//                "Concerts",
-//                1
-//            )
-//            return Single.just(event)
-//        }
         return eventRemoteDataSource.getEvent(eventId)
     }
 
@@ -43,28 +23,6 @@ class EventRepository @Inject constructor(
     }
 
     fun getUserEvents(userId : String?) : Observable<EventEntity> {
-//        val list = mutableListOf<EventEntity>()
-//        val image = "https://imgs.6sqft.com/wp-content/uploads/2019/10/02112923/Central-Park-fall-foliage-2019.jpg"
-//        val location = LocationEntity(
-//            12.0,
-//            10.0
-//        )
-//
-//        val event = EventEntity(
-//            "This is the event description",
-//            "2019-01-01",
-//            1,
-//            location,
-//            "https://imgs.6sqft.com/wp-content/uploads/2019/10/02112923/Central-Park-fall-foliage-2019.jpg",
-//            "2020-01-02",
-//            "Title",
-//            "Concerts",
-//            1
-//        )
-//        for (i in 1..10){
-//            list.add(event)
-//        }
-//        return Observable.fromIterable(list)
         val ids= if (userId == null){
             userLocalDataSource.getUser().flatMap {
                 eventRemoteDataSource.getUserEvents(it.toInt())
