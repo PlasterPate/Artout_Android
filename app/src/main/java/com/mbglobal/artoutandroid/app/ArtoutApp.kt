979218dcb8d63230a27994566fb0f1c47cbcdb98
Mobin Dariush <mobindh18@gmail.com>
2019-11-29 14:32:13 +0000

    Revert "Merge branch 'improvement/session' into 'dev'"
    This reverts merge request !6

--- app/src/main/java/com/mbglobal/artoutandroid/app/ArtoutApp.kt
+++ app/src/main/java/com/mbglobal/artoutandroid/app/ArtoutApp.kt
@@ -4,13 +4,6 @@ import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import com.mbglobal.artoutandroid.di.component.AppComponent
 import com.mbglobal.artoutandroid.di.component.DaggerAppComponent
-import timber.log.Timber.DebugTree
-import timber.log.Timber
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.mbglobal.artoutandroid.BuildConfig
-
-
 
 class ArtoutApp : DaggerApplication() {
 
@@ -18,9 +11,7 @@ class ArtoutApp : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(DebugTree())
-        }
+
         instance = this
     }
 
