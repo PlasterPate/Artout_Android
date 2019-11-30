0e09ec8ce8cf3e34d1ca04c7141f23149eecfa46
Mobin Dariush <dalisyron@gmail.com>
2019-11-30 17:04:00 +0330

    Refactor DI modules by layer to save Movahed and myself from committing suicide in the future =)

--- app/src/main/java/com/mbglobal/artoutandroid/di/module/ViewModelModule.kt
+++ app/src/main/java/com/mbglobal/artoutandroid/di/module/ViewModelModule.kt
@@ -1,8 +1,15 @@
 package com.mbglobal.artoutandroid.di.module
 
 import androidx.lifecycle.ViewModel
+import com.mbglobal.artoutandroid.app.MainViewModel
 import com.mbglobal.artoutandroid.di.annotation.ViewModelKey
+import com.mbglobal.artoutandroid.ui.discover.DiscoverViewModel
+import com.mbglobal.artoutandroid.ui.eventdetails.EventDetailsViewModel
 import com.mbglobal.artoutandroid.ui.eventlist.EventListViewModel
+import com.mbglobal.artoutandroid.ui.login.LoginViewModel
+import com.mbglobal.artoutandroid.ui.profile.ProfileViewModel
+import com.mbglobal.artoutandroid.ui.register.RegisterViewModel
+import com.mbglobal.artoutandroid.ui.timeline.TimelineViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -15,4 +22,38 @@ abstract class ViewModelModule {
     @ViewModelKey(EventListViewModel::class)
     abstract fun bindEventListViewModel(eventListViewModel: EventListViewModel): ViewModel
 
+    @Binds
+    @IntoMap
+    @ViewModelKey(EventDetailsViewModel::class)
+    abstract fun bindEventDetailsViewModel(eventDetailsViewModel: EventDetailsViewModel) : ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DiscoverViewModel::class)
+    abstract fun bindDiscoverViewModel(discoverViewModel: DiscoverViewModel) : ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(LoginViewModel::class)
+    abstract fun bindLoginViewModel(loginViewModel: LoginViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun bindMainViewModel(mainViewModel: MainViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ProfileViewModel::class)
+    abstract fun bindProfileViewModel(profileViewModel: ProfileViewModel) : ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(RegisterViewModel::class)
+    abstract fun bindRegisterViewModel(registerViewModel: RegisterViewModel) : ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(TimelineViewModel::class)
+    abstract fun bindTimelineViewModel(viewModel: TimelineViewModel) : ViewModel
 }
