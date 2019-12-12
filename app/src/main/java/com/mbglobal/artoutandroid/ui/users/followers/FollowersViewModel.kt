112b6e56d6294e88f19273b9daac43766a27f03b
Mobin Dariush <dalisyron@gmail.com>
2019-12-12 23:11:57 +0330

    Add FollowersViewModel

---
+++ app/src/main/java/com/mbglobal/artoutandroid/ui/users/followers/FollowersViewModel.kt
@@ -0,0 +1,41 @@
+package com.mbglobal.artoutandroid.ui.users.followers
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.mbglobal.artoutandroid.ui.base.BaseViewModel
+import com.mbglobal.data.datasource.SessionLocalDataSource
+import com.mbglobal.data.entity.user.UserEntity
+import com.mbglobal.data.repository.SocialRepository
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+class FollowersViewModel @Inject constructor(
+    private val socialRepository: SocialRepository,
+    private val sessionLocalDataSource: SessionLocalDataSource
+) : BaseViewModel() {
+
+    private val _followers: MutableLiveData<List<UserEntity>> = MutableLiveData()
+    val followers: LiveData<List<UserEntity>> = _followers
+
+    fun loadFollowers(userId: String?) {
+        val id = userId?.let {
+            Single.just(it)
+        } ?: sessionLocalDataSource.getSession()
+            .map {
+                it.userId
+            }
+
+        id.flatMap {
+            socialRepository.getUserFollowers(it)
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { followers: List<UserEntity> ->
+                _followers.postValue(followers)
+            }.also {
+                compositeDisposable.add(it)
+            }
+    }
+
+}
